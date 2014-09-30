77177e559aeb7fedeae2eb90f36a88ffa0ca6107
unknown <ulams99@VM974233.ile.mtl.qc.ca>
2014-09-30 15:10:57 -0400

    plugins versions

--- js/jqm-config.js
+++ js/jqm-config.js
@@ -7,6 +7,8 @@ $(document).bind("mobileinit", function () {
     $.mobile.hashListeningEnabled = false;
     $.mobile.pushStateEnabled = false;
 
+
+    // Swiping config
 	$.event.special.swipe.scrollSupressionThreshold = 11; // More than this horizontal displacement, and we will suppress scrolling.
 	$.event.special.swipe.horizontalDistanceThreshold = 30; // Swipe horizontal displacement must be more than this.
 	$.event.special.swipe.durationThreshold = 500;  // More time than this, and it isn't a swipe.
